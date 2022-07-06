9c08494646f3f905e583bca91daaf7536cb77388
Gabriel Garcia dos Anjos <gabrielgarcia.anjos250@gmail.com>
2022-07-05 22:54:42 -0300

    feat: config code for deploy

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://GabrielAnjos011.github.io/linktree-react
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:GabrielAnjos011/linktree-react.git main:gh-pages
